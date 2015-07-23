f66ed1c00b25433db87da6fd7fb2f288dd330130
Antoine Pultier <antoine.pultier@sintef.no>
2015-07-23 13:59:29 +0200

    :rocket: Uses streams to reduce memory usage and improve the performances

--- grafterizer-rest-light/server.js
+++ grafterizer-rest-light/server.js
@@ -3,10 +3,20 @@ var express = require('express'),
     morgan = require('morgan'),
     request = require('request'),
     cors = require('cors'),
-    bodyParser = require('body-parser');
+    mime = require('mime'),
+    contentDisposition = require('content-disposition'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    filesizeParser = require('filesize-parser');
+
+if (process.env.DEBUG) {
+    require('request-debug')(request);
+}
 
 var endpointOntotext = process.env.ONTOTEXT || "http://ontotext:8080";
 var endpointGraftwerk = process.env.GRAFTWERK || "http://graftwerk:8080";
+var maxPreviewSize = process.env.MAX_PREVIEW_SIZE ?
+    filesizeParser(process.env.MAX_PREVIEW_SIZE) : filesizeParser('10MiB');
 
 var app = express();
 
@@ -16,29 +26,40 @@ app.use(cors());
 
 var jsonParser = bodyParser.json();
 
-// Used to check the API access
+// To check the API status
 app.get('/', function(req, res){
     res.send("Hei");
 });
 
-var sendRequestError = function(err, response, body, res) {
-    res.status(response && response.statusCode ? response.statusCode : 500);
+// Returns information about the file, using the content-disposition header
+// Also returns default values as a failback (in CSV)
+var getAttachmentInfos = function(response) {
+    var defaultInfos = {
+        type: 'csv',
+        name: 'output',
+        filename: 'output.csv',
+        mime: 'text/csv'
+    };
 
-    if (body) {
-        res.contentType('text/plain');
-        res.send(body);
-    } else {
-        res.json({error: err});
-    } 
-};
-
-var genericSuccessCallback = function(err, response, body, res) {
-    if (err || (response && response.statusCode !== 200)) {
-        sendRequestError(err, response, body, res);
-    } else {
-        res.contentType('text/plain');
-        res.send(body);
+    if (!response.headers || !response.headers['content-disposition']) {
+        return defaultInfos;
     }
+
+    var disposition = contentDisposition.parse(response.headers['content-disposition']);
+
+    if (!disposition.parameters || !disposition.parameters.filename) {
+        return defaultInfos;
+    }
+
+    var filename = disposition.parameters.filename;
+    var ext = path.extname(filename);
+
+    return {
+        type: ext.slice(1),
+        name: path.basename(filename, ext),
+        filename: path.basename(filename),
+        mime: mime.lookup(ext)
+    };
 };
 
 var downloadRaw = function(req, res, callbackSuccess) {
@@ -61,53 +82,63 @@ var downloadRaw = function(req, res, callbackSuccess) {
         }
     }
 
-    request.get({
+    return request.get({
         url: endpointOntotext + "/catalog/distributions/file",
         headers: {
             'distrib-id': distributionUri,
             Authorization: auth
         }
-    }, function(err, response, body) {
-        if (err || (response && response.statusCode !== 200)) {
-            sendRequestError(err, response, body, res);
-        } else {
-            callbackSuccess(response, body); 
-        }
-    });
+    })
+    .on('error', function(err){
+        res.status(500).json({error: err});
+    })
 };
 
 app.get('/raw', function(req, res) {
-    downloadRaw(req, res, function(response, body){
-        res.contentType('text/plain');
-        res.send(body);
-    });
+    var stream = downloadRaw(req, res);
+    if (!stream) return;
+    stream.pipe(res);
 });
 
 app.get('/original', function(req, res) {
-    downloadRaw(req, res, function(response, body){
+    var stream = downloadRaw(req, res);
+    if (!stream) return;
+    stream.on('response', function(response){
+        if (!response || response.statusCode !== 200) {
+            stream.pipe(res);
+            return;
+        };
+
+        var streamInfos = getAttachmentInfos(response);
+
         request.post({
             url: endpointGraftwerk+"/evaluate/pipe",
-            json: true,
+            headers: {
+                'transfer-encoding': 'chuncked'
+            },
+            // json: true,
             formData: {
                 pipeline: {
-                    value: '(defpipe my-pipe [data-file] (-> (read-dataset data-file :format :csv)))',
+                    value: '(defpipe my-pipe [data-file] (-> (read-dataset data-file)))',
                     options: {
                         filename: 'pipeline.clj',
                         contentType: 'text/plain'
                     }
                 },
                 data: {
-                    value: body,
+                    value: stream,
                     options: {
-                        filename: 'data.csv',
-                        contentType: 'text/csv'
+                        filename: streamInfos.filename,
+                        contentType: streamInfos.mime,
+                        knownLength: 10000000000
                     }
                 },
                 command: req.query.command || 'my-pipe'
             }
-        }, function(err, response, body) {
-            genericSuccessCallback(err, response, body, res);
-        });
+        }).on('error', function(err) {
+            res.status(500).json({error: err});
+        }).pipe(res);
+
     });
 });
 
@@ -126,38 +157,54 @@ app.post('/preview', jsonParser, function(req, res) {
 
     var clojure = req.body.clojure;
 
-    downloadRaw(req, res, function(response, body){
+    var stream = downloadRaw(req, res);
+    if (!stream) return;
+    stream.on('response', function(response){
+        if (!response || response.statusCode !== 200) {
+            stream.pipe(res);
+            return;
+        };
+
         var type = req.body.transformationType === "graft" ? "graft" : "pipe";
 
+        var streamInfos = getAttachmentInfos(response);
+
         request.post({
-            url: endpointOntotext+"/dapaas-services/grafter/transformation/preview",
-            json: true,
+            // url: endpointOntotext+"/dapaas-services/grafter/transformation/preview",
+            url: endpointGraftwerk+"/evaluate/"+type,
+            // json: true,
             headers: {
-                "command": req.query.command || "my-pipe",
-                "transformation-type": req.query.transformationType || "pipe",
-                Authorization: auth
+                // "command": req.query.command || "my-pipe",
+                // "transformation-type": req.query.transformationType || "pipe",
+                // Authorization: auth,
+                'transfer-encoding': 'chuncked'
             },
             formData: {
-                "input-file": {
-                    value: body,
+                "data": {
+                    value: stream,
                     options: {
-                        filename: 'data.csv',
-                        contentType: 'text/csv'
+                        filename: streamInfos.filename,
+                        contentType: streamInfos.mime,
+                        knownLength: 10000000000
                     }
                 },
-                "transformation-code": {
+                "pipeline": {
                     value: clojure,
                     options: {
-                        filename: 'data.csv',
-                        contentType: 'text/csv'
+                        filename: 'pipeline.clj',
+                        contentType: 'text/plain'
                     }
-                }
+                },
+                command: req.query.command || ('my-'+type)
             } 
-        }, function(err, response, body) {
-            genericSuccessCallback(err, response, body, res);
-        });
+        }).on('error', function(err) {
+            res.status(500).json({error: err});
+        }).pipe(res);
     });
 });
+
+var downloadErrorText = '<h3>An error has occured.</h3>'
+    + '<p><a href="http://project.dapaas.eu/dapaas-contact-us">Please contact us.</a></p>';
 
 app.get('/download', function(req, res) {
 
@@ -174,53 +221,85 @@ app.get('/download', function(req, res) {
         return;
     }
 
-    downloadRaw(req, res, function(response, bodyFile){
-        request.get({
-            url: endpointOntotext + "/catalog/transformations/code/clojure",
-            headers: {
-                'transformation-id': transformationUri,
-                Authorization: auth
-            }
-        }, function(err, response, bodyClojure) {
-            if (err || (response && response.statusCode !== 200)) {
-                sendRequestError(err, response, body, res);
-                return;
-            }
+    request.get({
+        url: endpointOntotext + "/catalog/transformations/code/clojure",
+        headers: {
+            'transformation-id': transformationUri,
+            Authorization: auth
+        }
+    }, function(err, response, bodyClojure) {
 
-            request.post({
-                url: endpointGraftwerk+"/evaluate/graft",
-                json: true,
+        if (err || (response && response.statusCode !== 200)) {
+            res.status(500).send(downloadErrorText);
+            return;
+        };
+
+        var dataStream = downloadRaw(req, res);
+        if (!dataStream) return;
+
+        var type = req.query.type === "graft" ? "graft" : "pipe";
+
+        dataStream.on('response', function(response) {
+            if (!response || response.statusCode !== 200) {
+                res.status(500).send(downloadErrorText);
+                return;
+            };
+
+            var dataStreamInfos = getAttachmentInfos(response);
+           
+            var resultStream = request.post({
+                url: endpointGraftwerk+"/evaluate/"+type,
+                headers: {
+                    'transfer-encoding': 'chuncked',
+                    Accept: type === 'graft' ? 'application/n-triples' : 'text/csv'
+                },
                 formData: {
-                    pipeline: {
+                    "data": {
+                        value: dataStream,
+                        options: {
+                            filename: dataStreamInfos.filename,
+                            contentType: dataStreamInfos.mime,
+                            knownLength: 10000000000
+                        }
+                    },
+                    "pipeline": {
                         value: bodyClojure,
                         options: {
                             filename: 'pipeline.clj',
                             contentType: 'text/plain'
                         }
                     },
-                    data: {
-                        value: bodyFile,
-                        options: {
-                            filename: 'data.csv',
-                            contentType: 'text/csv'
-                        }
-                    },
-                    command: req.query.command || 'my-graft'
-                }
-            }, function(err, response, body) {
-
-                if (err || (response && response.statusCode !== 200)) {
-                    sendRequestError(err, response, body, res);
-                    return;
-                }
-
-                res.status(response.statusCode);
-                res.contentType('application/n-triples');
-                res.setHeader('Content-disposition', 'attachment; filename=output.nt');
-                res.send(body);
+                    command: req.query.command || ('my-'+type)
+                } 
             });
-        });
 
+            resultStream.on('error', function(err) {
+                res.status(500).send(downloadErrorText);
+            }).on('response', function(response){
+                if (!response || response.statusCode !== 200) {
+                    res.status(500).send(downloadErrorText);
+                    return;
+                };
+
+                delete response.headers['content-disposition'];
+                delete response.headers['content-type'];
+                delete response.headers['server'];
+
+                var filename = dataStreamInfos.name.replace(/[^a-zA-Z0-9_\-]/g,'')
+                    +"-processed";
+
+                if (type === 'graft') {
+                    res.contentType('application/n-triples');
+                    res.setHeader('content-disposition', 'attachment; filename='+filename+'.nt');
+                } else {
+                    res.contentType('text/csv');
+                    res.setHeader('content-disposition', 'attachment; filename='+filename+'.csv');
+                }
+
+                resultStream.pipe(res);
+            });
+
+        });
     });
 });
 
